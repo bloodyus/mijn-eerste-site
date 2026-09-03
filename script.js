3537b01adfa636ccb443d38de8438c10b2296f5b
bloodyus <166013084+bloodyus@users.noreply.github.com>
2026-09-03 16:11:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const facts = [
   "The first website ever made is still online.",
   "JavaScript was created in just 10 days.",
-  "Git was originally built by Linus Torvalds for Linux development.",
-  "Ik heb echt moeilijk veel trek in een cheeseburger!"
+  "Git was originally built by Linus Torvalds for Linux development."
 ];
 
 document.getElementById("factButton").addEventListener("click", function() {
