1973bb0271137fb50e802a6e08b6f48be6f3ce7c
bloodyus <166013084+bloodyus@users.noreply.github.com>
2026-09-04 15:09:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const facts = [
-  "The first website ever made is still online.",
-  "JavaScript was created in just 10 days.",
-  "Git was originally built by Linus Torvalds for Linux development.",
+  "Echt",
+  "EEN",
+  "Baller!",
   "Ik heb moeilijk veel trek in een cheeseburger."
 ];
 
